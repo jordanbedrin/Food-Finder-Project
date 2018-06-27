eb1d708dd829290008315aa87719d8690b3b50d9
dixsie <dwolm001@gmail.com>
2018-06-27 10:50:09 -0400

    Added styles to Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,56 @@
 import React from 'react';
-
+import './Home.css';
 
 class Home extends React.Component {
  render() {
    return (
-   <div>
+   <div className= "Homebkgd">
+    <div className = "HomebkgdOverlay"> 
+      </div>
 
-    <div class = "navbar">Effin' Food Finder</div>
-
-
-    <div class = "title">Fudge Bucket Food Finder</div>
-
-
-    <div class = "callToAction">Fudge it up!</div>
-
-
-    <div class = "landing"> </div>
-
-
-    <div class = "trending"> </div>
-
+    <div className = "Navbar">
+      <nav>
+        <ul>
+          <li><a class="waves-effect waves-teal btn-flat">Search</a></li>
+          <li>Favorites</li>
+          <li>Trending</li>
+          <li>Fudge It</li>
+        </ul>
+      </nav>
+    </div>
 
 
-    <div class = "trendingPics"> </div>
+    <div className = "CallToAction">
+      <div className="logo"> 
+        <img src="http://oi67.tinypic.com/13z0pqv.jpg" alt="logo pic"/> 
+      </div>
+     
+      <div className="Homeform">
+        <form>
+          <label> 
+            <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
+      
+    </div>
 
 
-    {/* <div class = ""> </div>
+    {/* <div className = "landing">dgfhh 
+    </div> */}
+      
 
-    <div class = ""> </div> */}
+    <div className = "trending"> </div>
 
 
 
+    <div className = "trendingPics"> </div>
+
+
+    {/* 
+
+    <div className = ""> </div> */}
 
    </div>
    )
